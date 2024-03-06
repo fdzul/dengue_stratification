46f44333e82c44712b939280ca11266deed882ba
fdzul <felipe.dzul.m@gmail.com>
2024-03-05 18:31:20 -0600

    This commit update the talk

--- talks/dengue_stratification/R_script/covariates_Lim_2023.R
+++ talks/dengue_stratification/R_script/covariates_Lim_2023.R
@@ -16,38 +16,38 @@ DiagrammeR::grViz("digraph {
                   
                   ##### fuentes de información
                   
-                  dataset [label = 'Bases de Datos',  fillcolor =  '#DB4437', color = 'white', fontcolor = 'white']
+                  dataset [label = 'Bases de Datos',  fillcolor =  '#C51162', color = 'white', fontcolor = 'white']
                   
                   ##### datasets
-                  ento [label = 'Vigilancia Entomológica',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  epi [label = 'Vigilancia Epidemiológica',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  clim [label = 'Datos Climáticos',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  demo [label = 'Datos Demográficos',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  socio [label = 'Datos Socioeconómicos',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  ecol [label = 'Datos Ecológicos',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
-                  env [label = 'Datos Ambientales',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white', taillabel = 'tail']
-                  spatio_temporal [label = 'Datos Spatio-Temporal',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
+                  ento [label = 'Vigilancia Entomológica',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  epi [label = 'Vigilancia Epidemiológica',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  clim [label = 'Datos Climáticos',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  demo [label = 'Datos Demográficos',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  socio [label = 'Datos Socioeconómicos',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  ecol [label = 'Datos Ecológicos',  fillcolor = '#880E4F', color = 'white', fontcolor = 'white']
+                  env [label = 'Datos Ambientales',  fillcolor = '#880E4F', color = 'white', fontcolor = 'white', taillabel = 'tail']
+                  spatio_temporal [label = 'Datos Spatio-Temporal',  fillcolor = '#880E4F', color = 'white', fontcolor = 'white']
                   
                   # 
                   # Analisis
-                  pca [label = 'PCA',  fillcolor =  '#BC4434', color = 'white', fontcolor = 'white']
-                  pca2 [label = 'PCA',  fillcolor =  '#BC4434', color = 'white', fontcolor = 'white']
+                  pca [label = 'PCA',  fillcolor =  '#CFD8DC', color = 'white', fontcolor = 'black']
+                  pca2 [label = 'PCA',  fillcolor =  '#CFD8DC', color = 'white', fontcolor = 'black']
                   
-                  cluster [label = 'Cluster Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  hotspots [label = 'Hotspots Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
+                  cluster [label = 'Cluster Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
+                  hotspots [label = 'Hotspots Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
                   
                   # indices
                   
-                  index_ento [label = 'Indicador Entomologico',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_epi [label = 'Indicador Epidemiológico',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_clim [label = 'Indicador Climático',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_demo [label = 'Indicador Demográfico',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_socio [label = 'Indicador Socioeconómico',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_ecol [label = 'Indicador Ecológico',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
-                  index_env [label = 'Indicaor Ambientales',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white', taillabel = 'tail']
-                  index_spatio_temporal [label = 'Indicador Spatio-Temporal',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
+                  index_ento [label = 'Indicador Entomologico',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_epi [label = 'Indicador Epidemiológico',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_clim [label = 'Indicador Climático',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_demo [label = 'Indicador Demográfico',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_socio [label = 'Indicador Socioeconómico',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_ecol [label = 'Indicador Ecológico',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
+                  index_env [label = 'Indicaor Ambientales',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white', taillabel = 'tail']
+                  index_spatio_temporal [label = 'Indicador Spatio-Temporal',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
                   
-                  index_compuesto [label = 'Indicador Compuesto',  fillcolor =  'DodgerBlue', color = 'white', fontcolor = 'white']
+                  index_compuesto [label = 'Indicador Compuesto',  fillcolor =  '#880E4F', color = 'white', fontcolor = 'white']
 
                   ##### define the relation
                   dataset -> {ento epi clim demo socio ecol env spatio_temporal} -> pca 
@@ -56,16 +56,17 @@ DiagrammeR::grViz("digraph {
                   pca2 -> index_compuesto
                   
                   ###
-                  dal [label = 'Dalenius & Hodge',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  hotspots2 [label = 'Hotspots Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  pl [label = 'Power Law Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
+                  dal [label = 'Dalenius & Hodge',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
+                  hotspots2 [label = 'Hotspots Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
+                  pl [label = 'Power Law Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'white']
                   #ensamble [label = 'Emsamble',  fillcolor = '#2731B5', color = 'white', fontcolor = 'white']
                   
                   
                   index_compuesto -> {dal hotspots2 pl} 
                   
                   
-}", heigh = "500px", width = "700px")
+}", heigh = "500px",
+width = "800px")
 
 
 #######
@@ -102,8 +103,8 @@ DiagrammeR::grViz("digraph {
                   pca [label = 'PCA',  fillcolor =  '#BC4434', color = 'white', fontcolor = 'white']
                   pca2 [label = 'PCA',  fillcolor =  '#BC4434', color = 'white', fontcolor = 'white']
                   
-                  cluster [label = 'Cluster Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  hotspots [label = 'Hotspots Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
+                  cluster [label = 'Cluster Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'black']
+                  hotspots [label = 'Hotspots Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'black']
                   
                   # indices
                   
@@ -125,9 +126,9 @@ DiagrammeR::grViz("digraph {
                   pca2 -> index_compuesto
                   
                   ###
-                  dal [label = 'Dalenius & Hodge',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  hotspots2 [label = 'Hotspots Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
-                  pl [label = 'Power Law Análisis',  fillcolor =  'orange', color = 'white', fontcolor = 'black']
+                  dal [label = 'Dalenius & Hodge',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'black']
+                  hotspots2 [label = 'Hotspots Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'black']
+                  pl [label = 'Power Law Análisis',  fillcolor =  '#1B5E20', color = 'white', fontcolor = 'black']
                   #ensamble [label = 'Emsamble',  fillcolor = '#2731B5', color = 'white', fontcolor = 'white']
                   
                   
